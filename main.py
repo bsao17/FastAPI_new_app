3422aba553716732475fa63e71c9314a5bbfa19d
Bruno M <declic62gmail.com>
2023-10-17 18:31:02 +0200

    feat: query args

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from enum import Enum
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 
 class Person(str, Enum):
     John = "John"
     Jane = "Jane"
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
 
 
 @app.get("/{person}")
@@ -17,5 +24,3 @@ async def persons(person: Person):
         return {"message": f"Hello {person} Doe"}
     else:
         return {"message": f"Hello {person} Doe"}
-
-
