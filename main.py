1ca7786187f3e9b84b3736833da6b1e962e90143
Bruno M <declic62gmail.com>
2023-10-17 18:39:48 +0200

    feat:add Docstring

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ class Person(str, Enum):
     Jane = "Jane"
 
 
+"""
+Retrieves items from the fake_items_db based on the given skip and limit parameters.
+
+Parameters:
+    skip (Union[int, None], optional): The number of items to skip. Defaults to 0.
+    limit (Union[int, None], optional): The maximum number of items to retrieve. Defaults to 10.
+
+Returns:
+    List: A list of items retrieved from fake_items_db.
+"""
+
+
 @app.get("/items/")
 async def read_item(skip: Union[int, None] = 0, limit: Union[int, None] = 10):
     return fake_items_db[skip: skip + limit]
@@ -20,6 +32,15 @@ async def read_item(skip: Union[int, None] = 0, limit: Union[int, None] = 10):
 
 @app.get("/{person}")
 async def persons(person: Person):
+    """
+    A function that handles requests to the "/{person}" endpoint.
+
+    Parameters:
+    - person (Person): The person object representing the person being requested.
+
+    Returns:
+    - dict: A dictionary containing the response message.
+    """
     if request is person.John:
         return {"message": f"Hello {person} Doe"}
     else:
