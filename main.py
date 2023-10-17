faaf91d087bc391e1199e485c1f094293f80d18b
Bruno M <declic62gmail.com>
2023-10-17 19:23:58 +0200

    feat:add read_user_item function

--- main.py
+++ main.py
@@ -63,3 +63,13 @@ async def persons(person: Person):
 def iterable(item: int):
     items = [x for x in range(item)]
     return {"items": items}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: int, q: str | None = None, short: bool = False):
+    item = {"user_id": user_id, "item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        return {"short": "short value is to False !"}
+    return item
